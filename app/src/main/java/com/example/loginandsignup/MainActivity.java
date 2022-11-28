2db851eb74af48420def4309b9ac66f5276ff0e5
simran mehra <simranmehra.iv@gmail.com>
2022-11-27 21:29:14 -0500

    initial setup

---
+++ app/src/main/java/com/example/loginandsignup/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.loginandsignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button register;
+    EditText user;
+    EditText pass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
