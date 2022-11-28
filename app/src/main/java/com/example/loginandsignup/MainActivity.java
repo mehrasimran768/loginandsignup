2da0ef1018ebe5f08a38f1b211d57cbe16e27a55
simran mehra <simranmehra.iv@gmail.com>
2022-11-27 21:38:35 -0500

    creating the UI

--- app/src/main/java/com/example/loginandsignup/MainActivity.java
+++ app/src/main/java/com/example/loginandsignup/MainActivity.java
@@ -14,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        user = findViewById(R.id.user);
+        pass = findViewById(R.id.pass);
     }
 }
